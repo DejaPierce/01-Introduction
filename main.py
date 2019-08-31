ff3873a5df6742b6c68ffc054150a7664ee172ff
Deja Pierce <dejapierce@Dejas-MacBook-Air.local>
2019-08-30 23:51:22 -0400

    Introduction

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Deja Pierce!')
+print('My favorite game is Kingdom Hearts')
+print('My only concern is how difficult the coding is going to get')
+print('Im excited about being able to make games')
+print('My username is Deja Pierce')
+print('My url is DejaPierce')
